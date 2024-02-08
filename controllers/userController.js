2d6e546384f928f15e741b3adc76adb8f316194e
Mustafa Zorlu <mzorlu3579@gmail.com>
2024-02-08 21:15:49 +0300

    some changes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 import User from "../models/userModel.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const createUser = async (req, res) => {
     try {
@@ -20,7 +21,7 @@ const createUser = async (req, res) => {
 const loginUser = async (req, res) => {
     try {
         const { username, password } = req.body;
-        const user = await User.findOne({username});
+        const user = await User.findOne({ username });
         let same = false;
         if (user) {
             same = await bcrypt.compare(password, user.password);
@@ -32,7 +33,10 @@ const loginUser = async (req, res) => {
         }
 
         if (same) {
-            res.status(200).send("You are logged in.");
+            res.status(200).json({
+                user,
+                token:createToken(user._id)
+            });
         } else {
             res.status(401).json({
                 succeded: false,
@@ -45,6 +49,12 @@ const loginUser = async (req, res) => {
             error,
         });
     }
+};
+
+const createToken = (userId) => {
+    return jwt.sign({userId}, process.env.JWT_SECRET,{
+        expiresIn:"1d"
+    });
 };
 
 export { createUser, loginUser };
